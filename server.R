4775c4f5033a82fcfe0a68a10220c2a016ed1c53
Murat Koptur <mkoptur3@gmail.com>
2019-07-12 17:37:10 +0300

    some fixes and theme change

--- server.R
+++ server.R
@@ -5,14 +5,12 @@ library(tibbletime)
 library(tidyverse)
 
 shinyServer(function(input, output, session) {
-  
-  dataInput <- reactive(
-    {
-      req(input$inputFile)
-      ts_data <- read_csv(input$inputFile$datapath, col_names = input$headers)
-      return(ts_data)
-    }
-  )
+  dataInput <- reactive({
+    req(input$inputFile)
+    ts_data <-
+      read_csv(input$inputFile$datapath, col_names = input$headers)
+    return(ts_data)
+  })
   
   output$table <- renderTable({
     data_raw <- dataInput()
@@ -30,41 +28,35 @@ shinyServer(function(input, output, session) {
   observeEvent(dataInput(), {
     updateSelectInput(session, "dateCol", choices = names(dataInput()))
   })
-    
-  make_ts <- reactive(
-    {
-      data_raw <- dataInput()
-      dataCol <- input$dataCol
-      dateCol <- input$dateCol
-      startDate <- input$startDate
-      freq <- input$freq
-      data <- pull(data_raw, dataCol)
-      date <- pull(data_raw, dateCol)
-      ts_data <- ts(data, 
-                    start = startDate,
-                    frequency = as.integer(freq))
-      return(ts_data)
-    }
-  )
   
-  output$decomp <- renderPlot(
-    {
-      ts_data <- make_ts()
-      return(plot(stl(ts_data, "periodic")))
-    }
-  )
+  make_ts <- reactive({
+    data_raw <- dataInput()
+    dataCol <- input$dataCol
+    dateCol <- input$dateCol
+    startDate <- input$startDate
+    freq <- input$freq
+    data <- pull(data_raw, dataCol)
+    date <- pull(data_raw, dateCol)
+    ts_data <- ts(data,
+                  start = startDate,
+                  frequency = as.integer(freq))
+    return(ts_data)
+  })
   
+  output$decomp <- renderPlot({
+    ts_data <- make_ts()
+    return(plot(stl(ts_data, "periodic")))
+  })
   
-  output$anomalies <- renderPlot(
-  {
-      ts_data <- make_ts()
-      print(as.vector(ts_data))
-      anom_data <<- AnomalyDetectionVec(x = as.vector(ts_data),
-                                        max_anoms = 0.02,
-                                        period = as.integer(input$freq),
-                                        direction = 'both', 
-                                        plot = T)
-      return(anom_data$plot)
-  }
-  )
+  output$anomalies <- renderPlot({
+    ts_data <- make_ts()
+    anom_data <<- AnomalyDetectionVec(
+      x = as.vector(ts_data),
+      max_anoms = 0.02,
+      period = as.integer(input$freq),
+      direction = 'both',
+      plot = T
+    )
+    return(anom_data$plot)
+  })
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,34 @@
 library(shiny)
+library(shinythemes)
 
 shinyUI(fluidPage(
+  theme = shinytheme("flatly"),
   titlePanel("Anomaly Detection"),
-  sidebarLayout(sidebarPanel(
-    fileInput(
-      "inputFile",
-      "Choose CSV file",
-      multiple = FALSE,
-      accept = c("text/csv",
-                 "text/comma-separated-values,text/plain",
-                 ".csv"),
-    width = NULL
-  ),
-    checkboxInput("headers", "Headers?"),
-    dateInput("startDate", "Start Date"),
-    selectInput("freq", "Frequency", as.list(c(1, 4, 12, 52))),
-    selectInput("dateCol", "Date Column", ""),
-    selectInput("dataCol", "Data Column", "")
-
-  ),
-  mainPanel(h3("Data"),tableOutput("table"),
-            h3("STL Decomposition"),plotOutput("decomp"),
-            h3("Anomalies"), plotOutput("anomalies"))
-)))
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(
+        "inputFile",
+        "Choose CSV file",
+        multiple = FALSE,
+        accept = c("text/csv",
+                   "text/comma-separated-values,text/plain",
+                   ".csv"),
+        width = NULL
+      ),
+      checkboxInput("headers", "Headers?"),
+      dateInput("startDate", "Start Date"),
+      selectInput("freq", "Frequency", as.list(c(1, 4, 12, 52))),
+      selectInput("dateCol", "Date Column", ""),
+      selectInput("dataCol", "Data Column", "")
+      
+    ),
+    mainPanel(
+      h3("Data"),
+      tableOutput("table"),
+      h3("STL Decomposition"),
+      plotOutput("decomp"),
+      h3("Anomalies"),
+      plotOutput("anomalies")
+    )
+  )
+))
